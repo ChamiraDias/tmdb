941cdedeb29b4f98e5d4f1b0685da176c4c423e8
chamira.dias <mail.chamira@gmail.com>
2018-09-20 16:11:43 +0530

    no message

--- controller/cron.runner.js
+++ controller/cron.runner.js
@@ -1,15 +1,15 @@
-var cron = require('cron');
+// var cron = require('cron');
 
-let controller = require('../controller/movie.data.controller');
+// let controller = require('../controller/movie.data.controller');
 
-var job1 = new cron.CronJob({
-    cronTime: "*/15 * * * * *",
-    onTick: function() {
-        controller.getMovieCrewData();
-        console.log(
-            "\n----------------------------------------------Running Event Monitor--------------------------------------\n"
-        );
-    },
-    start: true
-        ///
-});
+// var job1 = new cron.CronJob({
+//     cronTime: "*/15 * * * * *",
+//     onTick: function() {
+//         controller.getMovieCrewData();
+//         console.log(
+//             "\n----------------------------------------------Running Event Monitor--------------------------------------\n"
+//         );
+//     },
+//     start: true
+//         ///
+// });
